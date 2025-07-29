6597a1c785d80d8863a22ccfeada681886a641da
coolboy19521 <coolboy19521@gmail.com>
2025-07-29 11:25:46 +0400

    Camera streaming via flask added

--- src/main.py
+++ src/main.py
@@ -1,38 +1,28 @@
-#!/usr/bin/env python3
-'''Animates distances and measurment quality'''
-from rplidar import RPLidar
-import matplotlib.pyplot as plt
-import numpy as np
-import matplotlib.animation as animation
+from flask import Flask, render_template, Response
+from picamera2 import Picamera2
+import cv2
 
-PORT_NAME = '/dev/ttyUSB0'
-DMAX = 4000
-IMIN = 0
-IMAX = 50
+app = Flask(__name__)
 
-def update_line(num, iterator, line):
-    scan = next(iterator)
-    offsets = np.array([(np.radians(meas[1]), meas[2]) for meas in scan])
-    line.set_offsets(offsets)
-    intens = np.array([meas[0] for meas in scan])
-    line.set_array(intens)
-    return line,
+picam2 = Picamera2()
+picam2.configure(picam2.create_video_configuration(main={"format": 'XRGB8888', "size": (640, 480)}, controls={"FrameDurationLimits": (66666, 66666)}))
+picam2.start()
 
-def run():
-    lidar = RPLidar(PORT_NAME)
-    fig = plt.figure()
-    ax = plt.subplot(111, projection='polar')
-    line = ax.scatter([0, 0], [0, 0], s=5, c=[IMIN, IMAX],
-                           cmap=plt.cm.Greys_r, lw=0)
-    ax.set_rmax(DMAX)
-    ax.grid(True)
+def generate_frames():
+    while True:
+        frame = picam2.capture_array()
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-    iterator = lidar.iter_scans()
-    ani = animation.FuncAnimation(fig, update_line,
-        fargs=(iterator, line), interval=50)
-    plt.show()
-    lidar.stop()
-    lidar.disconnect()
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    run()
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=False)
